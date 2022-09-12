994681e256f3497b20b488f56fa938c514f64748
Raul Escobar-Garcia <raul3@rauls-pc.localdomain>
2022-09-12 15:44:26 -0500

    saving

--- A1/src/main.cpp
+++ A1/src/main.cpp
@@ -25,7 +25,33 @@ int keyPresses[256] = {0}; // only for English keyboards!
 
 shared_ptr<Program> prog;
 shared_ptr<Camera> camera;
-shared_ptr<Shape> shape;
+shared_ptr<Shape> body1;
+shared_ptr<Shape> prop1;
+shared_ptr<Shape> body2;
+shared_ptr<Shape> prop2;
+
+double t, dt;
+
+glm::mat4 B, G;
+float c;
+
+
+struct Object { 
+	Object(shared_ptr<Shape> shape, glm::vec3 position, glm::quat rotation, float scale, bool rotate=false): 
+		shape(shape), position(position), rotation(rotation), scale(scale), rotate(rotate) {};
+	shared_ptr<Shape> shape;
+	glm::vec3 position;
+	glm::quat rotation;
+	float scale;
+	bool rotate;
+};
+
+struct Helicopter {
+	Helicopter() {};
+};
+
+vector<Object> objects;
+vector<glm::vec3> keyframes;
 
 static void error_callback(int error, const char *description)
 {
@@ -68,6 +94,10 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 	}
 }
 
+int mod(int a, int b) {
+	return a < 0 ? mod(a+b, b) : a%b;
+}
+
 static void init()
 {
 	GLSL::checkVersion();
@@ -76,6 +106,12 @@ static void init()
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	// Enable z-buffer test
 	glEnable(GL_DEPTH_TEST);
+
+	c  = 1.0/2.0f;
+	B[0] = glm::vec4(0.0f, 2.0f, 0.0f, 0.0f);
+	B[1] = glm::vec4(-1.0f, 0.0f, 1.0f, 0.0f);
+	B[2] = glm::vec4(2.0f, -5.0f, 4.0f, -1.0f);
+	B[3] = glm::vec4(-1.0f, 3.0f, -3.0f, 1.0f);
 
 	keyPresses[(unsigned)'c'] = 1;
 	
@@ -96,23 +132,56 @@ static void init()
 	
 	camera = make_shared<Camera>();
 	
-	shape = make_shared<Shape>();
-	shape->loadMesh(RESOURCE_DIR + "helicopter_body1.obj");
-	shape->init();
+	body1 = make_shared<Shape>();
+	body1->loadMesh(RESOURCE_DIR + "helicopter_body1.obj");
+	body1->init();
+	objects.push_back(Object(body1, glm::vec3(0.0f, 0.0f, 0.0f),glm::quat(1.0f, 0.0f, 0.0f, 0.0f), 1.0f));
+
+	prop1 = make_shared<Shape>();
+	prop1->loadMesh(RESOURCE_DIR + "helicopter_prop1.obj");
+	prop1->init();
+	objects.push_back(Object(prop1, glm::vec3(0.0f, 0.0f, 0.0f),glm::angleAxis(0.0001f, glm::vec3(0.0f, 0.4819f, 0.0f)), 1.0f, true));
+
+	body2 = make_shared<Shape>();
+	body2->loadMesh(RESOURCE_DIR + "helicopter_body2.obj");
+	objects.push_back(Object(body2, glm::vec3(0.0f, 0.0f, 0.0f),glm::quat(1.0f, 0.0f, 0.0f, 0.0f), 1.0f));
+	body2->init();
+
+	prop2 = make_shared<Shape>();
+	prop2->loadMesh(RESOURCE_DIR + "helicopter_prop2.obj");
+	prop2->init();
+	objects.push_back(Object(prop2, glm::vec3(0.0f, 0.0f, 0.0f),glm::angleAxis(0.0001f, glm::vec3(0.6228f, 0.1179f, 0.1365f)), 1.0f, true));
 	
+	keyframes.push_back(glm::vec3(-1.0f, 0.0f, 1.0f));
+	keyframes.push_back(glm::vec3(0.0f, 3.0f, -1.0f));
+	keyframes.push_back(glm::vec3(7.0f, 0.0f, 2.0f));
+	keyframes.push_back(glm::vec3(0.0f, -2.0f, -2.0f));
+	keyframes.push_back(glm::vec3(-8.0f, -8.0f, 0.0f));
+
 	// Initialize time.
 	glfwSetTime(0.0);
-	
+	t = 0.0;
 	// If there were any OpenGL errors, this will print something.
 	// You can intersperse this line in your code to find the exact location
 	// of your OpenGL error.
 	GLSL::checkError(GET_FILE_LINE);
 }
 
+void update() {
+	t = glfwGetTime();
+	for (Object& object: objects) {
+		if (object.rotate) {
+			float angle = t;
+			//object.rotation *= glm::angleAxis((float) 0.01f, glm::axis(object.rotation));
+		}
+	}
+}
+
 void render()
 {
 	// Update time.
-	double t = glfwGetTime();
+	dt = glfwGetTime() - t;
+	t += dt;
 	
 	// Get current frame buffer size.
 	int width, height;
@@ -144,13 +213,35 @@ void render()
 	camera->applyProjectionMatrix(P);
 	MV->pushMatrix();
 	camera->applyViewMatrix(MV);
+
+	float u = std::fmod(t, (float)keyframes.size());
+
+	int size = (int)keyframes.size();
+	G[0] = glm::vec4(keyframes[mod((int)u - 1, size)], 0.0f);
+	G[1] = glm::vec4(keyframes[mod((int)u , size)], 0.0f);
+	G[2] = glm::vec4(keyframes[mod((int)u + 1, size)], 0.0f);
+	G[3] = glm::vec4(keyframes[mod((int)u + 2, size)], 0.0f);
 	
-	prog->bind();
-	glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, glm::value_ptr(P->topMatrix()));
-	glUniformMatrix4fv(prog->getUniform("MV"), 1, GL_FALSE, glm::value_ptr(MV->topMatrix()));
-	glUniform3f(prog->getUniform("kd"), 1.0f, 0.0f, 0.0f);
-	shape->draw(prog);
-	prog->unbind();
+	float k = u - std::floor(u);
+	glm::vec4 uVec(1.0f, k, k*k, k*k*k);
+	// Compute position at u
+	glm::vec4 offset = c * G*(B*uVec);
+	
+
+	for (const Object& object : objects) {
+		MV->pushMatrix();
+			
+			MV->translate(object.position + glm::vec3(offset));
+			MV->scale(object.scale, object.scale, object.scale);
+			MV->multMatrix(glm::mat4_cast(object.rotation));
+			prog->bind();
+			glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, glm::value_ptr(P->topMatrix()));
+			glUniformMatrix4fv(prog->getUniform("MV"), 1, GL_FALSE, glm::value_ptr(MV->topMatrix()));
+			glUniform3f(prog->getUniform("kd"), 1.0f, 0.0f, 0.0f);
+			object.shape->draw(prog);
+			prog->unbind();
+		MV->popMatrix();
+	}
 	
 	// Draw the frame and the grid with OpenGL 1.x (no GLSL)
 	
@@ -177,8 +268,31 @@ void render()
 	glVertex3f(0, 0, 0);
 	glVertex3f(0, 0, 1);
 	glEnd();
+
+	glBegin(GL_LINE_STRIP);
+	if(keyPresses[(unsigned)'k'] % 2) {
+		glColor3f(0, 1, 0);
+		for (int j = -1; j < ((int)keyframes.size()) - 1; ++j) {
+			
+			int size = (int)keyframes.size();
+			G[0] = glm::vec4(keyframes[mod(j, size)], 0.0f);
+			G[1] = glm::vec4(keyframes[mod(j+1, size)], 0.0f);
+			G[2] = glm::vec4(keyframes[mod(j+2, size)], 0.0f);
+			G[3] = glm::vec4(keyframes[mod(j+3, size)], 0.0f);
+			
+			
+			for(int u_i = 0; u_i < 11; ++u_i) {
+				float u = u_i / 10.0f;
+				glm::vec4 uVec(1.0f, u, u*u, u*u*u);
+				// Compute position at u
+				glm::vec4 p = c * G*(B*uVec);
+				glVertex3f(p.x, p.y, p.z);
+			}
+		}
+	}
+	glEnd();
+
 	glLineWidth(1);
-	
 	// Draw grid
 	float gridSizeHalf = 20.0f;
 	int gridNx = 40;
@@ -229,7 +343,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	// Create a windowed mode window and its OpenGL context.
-	window = glfwCreateWindow(640, 480, "YOUR NAME", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Raul Escobar", NULL, NULL);
 	if(!window) {
 		glfwTerminate();
 		return -1;
@@ -260,6 +374,8 @@ int main(int argc, char **argv)
 	// Loop until the user closes the window.
 	while(!glfwWindowShouldClose(window)) {
 		if(!glfwGetWindowAttrib(window, GLFW_ICONIFIED)) {
+			// update
+			update();
 			// Render scene.
 			render();
 			// Swap front and back buffers.
